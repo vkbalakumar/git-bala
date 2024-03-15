25d16e66b1f1addd5809d2aabe68fea5e765f872
vkbalakumar <vkbalakumar@gmail.com>
2024-03-15 16:08:37 +0530

    second commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+# variable "ami" {
+#   default = "ami-03bb6d83c60fc5f7c"
+# }
+
+# variable "instance_type" {
+#   default = "t2.micro"
+# }
+
+# variable "ingress_port" {
+#   default = [22, 80]
+# }
